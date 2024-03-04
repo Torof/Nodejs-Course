f9ef7fc4d47e480bc30e8024096621691eb574e4
torof <scottpanam@protonmail.com>
2024-03-04 11:28:18 +0900

    basic app, change promises to async/await

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
+
+//create new user
 app.post('/user', (req, res) => {
     const user = new User(req.body)
 
@@ -16,6 +18,7 @@ app.post('/user', (req, res) => {
     .catch((error) => res.status(400).send(error))
 })
 
+//create new task
 app.post("/task", (req, res) => {
     const task = new Task(req.body)
 
@@ -23,6 +26,54 @@ app.post("/task", (req, res) => {
     .then(() => res.status(201).send(task))
     .catch((error) => res.status(400).send(error))
 })
+
+//fetch all users
+app.get("/users", (req, res) => {
+    User.find({})
+    .then(users => res.send(users))
+    .catch( error => res.status(500).send(error))
+})
+
+//fetch all tasks
+app.get("/tasks", (req, res) =>{
+    Task.find({})
+    .then(tasks => {
+        if(tasks.length == 0) return res.status(404).send()
+        res.send(tasks)
+    })
+    .catch(() => res.status(500).send())
+})
+
+
+//get user by id
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id)
+    .then( user => {
+        if(!user){
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    })
+    .catch(() => res.status(500).send())
+})
+
+
+//get task by id
+app.get("/task/:id", (req, res) => {
+    const taskId = req.params.id
+
+    Task.findById(taskId)
+    .then((task) => {
+        if(!task) return req.status(404).send()
+
+        res.send(task)
+    })
+    .catch(() => req.status(500).send())
+})
+
 
 app.listen(port, () => {
     console.log("server is up on port " + port)

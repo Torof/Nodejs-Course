414795fd7a02b14843eaa15fc0789314eb866618
torof <scottpanam@protonmail.com>
2024-03-15 18:08:48 +0900

    auto cropping & formatting images

--- task-manager/src/routers/user.router.js
+++ task-manager/src/routers/user.router.js
@@ -3,6 +3,7 @@ const User = require("../models/user.model")
 const auth = require('../middleware/auth.middleware')
 const router = new express.Router()
 const multer = require('multer')
+const sharp = require('sharp')
 
 
 //create new user
@@ -110,7 +111,9 @@ const uploadAvatar = multer({
 
 //upload picture
 router.post('/users/me/avatar', auth, uploadAvatar.single('avatar'), async (req, res) => {
-    req.user.avatar = req.file.buffer
+    const buffer = await sharp(req.file.buffer).resize({width: 250, height: 250 }).png().toBuffer()
+    
+    req.user.avatar = buffer
     await req.user.save()
     res.send()
 }, (error, req, res, next) => {
@@ -135,7 +138,7 @@ router.get('/users/:id/avatar', async (req, res) => {
 
         if(!user || !user.avatar) throw new Error()
 
-        res.set('Content-Type', 'image/jpg')
+        res.set('Content-Type', 'image/png')
         res.send(user.avatar)
 
     } catch (error) {

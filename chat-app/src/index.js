675ed71597eed3a3e8585bab360a66fe8d1980c3
torof <scottpanam@protonmail.com>
2024-03-30 09:20:00 +0900

    basic chat app with socket.io

---
+++ chat-app/src/index.js
@@ -0,0 +1,28 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname, '../public');
+
+// Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+io.on('connection', (socket) => {
+    console.log('new web socket connection')
+
+    socket.emit('message', 'welcome!')
+
+    socket.on('sendMessage', (message) => {
+        io.emit('message',message)
+    })
+})
+
+server.listen(port, () => {
+    console.log('server is up on port ' + port)
+})
